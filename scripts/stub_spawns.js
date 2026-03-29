7d34e75fc7f892ae308e19265280d66daca4d5b3
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2026-03-29 10:01:44 -0400

    created stub script

---
+++ scripts/stub_spawns.js
@@ -0,0 +1,16 @@
+import { Glob } from "bun";
+
+const glob = new Glob("**/*.json");
+const path = "./datapacks/spawn_pool_stub_all";
+const stubJson = JSON.stringify({
+    "enabled": false
+}, undefined, 2);
+
+for (const filePath of glob.scanSync(path)) {
+  try {
+    await Bun.write(`${path}/${filePath}`, stubJson);
+    console.log(`Modified file: ${filePath}`);
+  } catch (error) {
+    console.error(`Error processing file ${filePath}:`, error);
+  }
+}
