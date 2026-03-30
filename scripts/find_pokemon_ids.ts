fe7d49860ab6bdeaccca23e0c0b587acb11f993c
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2026-03-29 22:38:35 -0400

    updated to find legendary ids, maybe lookup dex numbers?

--- scripts/find_pokemon_ids.ts
+++ scripts/find_pokemon_ids.ts
@@ -1,29 +1,21 @@
 import { Glob } from "bun";
 
 const glob = new Glob("**/*.json");
-const path = "../datapacks/spawn_pool_stub_all";
+const path = "../reference_data/legendary_spawns_atm/spawn_pool_world";
 const outputPath = "./pokemon_ids.csv";
 
-const pokemonIds = [];
-const csvLines = ["dex,id,implemented"];
+const csvLines = ["name,implemented"];
 
 for (const filePath of glob.scanSync(path)) {
   try {
-    const match = /([0-9]{4})_([a-z]+?)\.json/g.exec(filePath);
+    const match = /^([a-z]+)\.json/g.exec(filePath);
     if (match) {
-      const [_, _dex, id] = match;
-      const dex = parseInt(_dex || "1");
-      pokemonIds[dex - 1] = id;
+      const [_, id] = match;
+      csvLines.push(`${id},TRUE`);
     }
   } catch (error) {
     console.error(`Error processing file ${filePath}:`, error);
   }
-}
-for (let i = 0; i < pokemonIds.length; i++) {
-  const dex = i + 1;
-  const id = pokemonIds[i];
-  const implemented = id ? "yes" : "no";
-  csvLines.push(`${dex},${id || ""},${implemented}`);
 }
 
 Bun.write(outputPath, csvLines.join("\n"));
