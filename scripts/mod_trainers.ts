7352102942557316232c6701c10b94efe3fdb6f5
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2026-04-28 15:24:18 -0400

    removed required trainers

--- scripts/mod_trainers.ts
+++ scripts/mod_trainers.ts
@@ -1,18 +1,19 @@
 import { Glob } from "bun";
 
 const glob = new Glob("**/*.json");
-const path = "./datapacks/pokeblock_datapack/data/rctmod/trainers";
+const path = "./datapacks/pokeblock_datapack/data/rctmod/mobs/trainers";
 
 for (const filePath of glob.scanSync(path)) {
   try {
     const content = await Bun.file(`${path}/${filePath}`)
       .json()
       .catch(() => null);
-    content.battleFormat = "GEN_9_DOUBLES";
-
-    const json = JSON.stringify(content, undefined, 2);
-    await Bun.write(`${path}/${filePath}`, json);
-    console.log(`Modified file: ${filePath}`);
+    if (content?.optional === false) {
+      content.optional = true;
+      const json = JSON.stringify(content, undefined, 2);
+      await Bun.write(`${path}/${filePath}`, json);
+      console.log(`Modified file: ${filePath}`);
+    }
   } catch (error) {
     console.error(`Error processing file ${filePath}:`, error);
   }
